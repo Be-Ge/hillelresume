cbc2a9109774a83f7c435507a36081fd536b213a
Anastasiia Buryak <be.ge.instagram@gmail.com>
2020-03-06 20:23:22 +0200

    add media about me

--- js/main.js
+++ js/main.js
@@ -5,3 +5,17 @@ $(document).ready(function(){
   });
 
 $('.single-item').slick();
+
+// burger
+const burgerOpen = document.querySelector('.burger-open');
+const burgerClose = document.querySelector('.burger-close');
+const windowMenu = document.querySelector('.window-menu');
+
+burgerOpen.onclick = (e) => {
+  windowMenu.classList.add('show')
+  document.body.classList.add('overflow')
+};
+burgerClose.onclick = (e) => {
+  windowMenu.classList.remove('show')
+  document.body.classList.remove('overflow')
+};
